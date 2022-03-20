fba1ffe98f6111497ee417f2ab74a2341a3c06cd
MorningSol <98545619+MorningSol@users.noreply.github.com>
2022-03-20 17:15:21 -0400

    change color to present/past/future

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,40 @@
-var currentDayEl =$("#currentDay");
-
+var currentDayEl = $("#currentDay");
+var timeBlocksEl = $(".time-block");
 
 
 currentDayEl.text(moment().format('dddd, MMMM Do'));
 
+
+
+    timeBlocksEl.each(function(){
+        var time = $(this).data("time");
+        
+        if (moment().format("H") == time){
+            $(this).addClass("present")
+            console.log("present")
+        }
+        else if (moment().format("H") > time){
+            $(this).addClass("past")
+            console.log("past")
+        }  
+        else if(moment().format("H") < time){
+            $(this).addClass("future")
+            console.log("future")
+        }
+
+    });
+    
+        
+    
+    
+
+    
+
+
+
+
+
+    
+
+
+ 
